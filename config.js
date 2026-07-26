a3ca4164bf4dadfde4e32bce59f470c9476d69bd
MaorLasri <39355371+MaorLasri@users.noreply.github.com>
2026-07-26 14:17:54 -0700

    Add Supabase client configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+window.BEAUTIX_CONFIG = {
+  supabaseUrl: "https://bkosuztxdgvfhykzuigx.supabase.co",
+  supabasePublishableKey: "sb_publishable_wXoQC8djBvfiLYK1eX-h_Q_uFnGTAPB",
+  reportRpc: "get_business_status_report",
+  refreshIntervalMs: 60000
+};
