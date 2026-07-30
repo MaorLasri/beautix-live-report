1fc951167eabdaf847925a6bce99c39eb7b870bb
MaorLasri <39355371+MaorLasri@users.noreply.github.com>
2026-07-30 03:02:53 -0700

    Add marketing conversion report and refresh sales data
    * Add marketing and conversion report page
    
    * Style marketing conversion report
    
    * Render marketing conversion analytics
    
    * Use report v2 with complete sales history

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BEAUTIX_CONFIG = {
   supabaseUrl: "https://bkosuztxdgvfhykzuigx.supabase.co",
   supabasePublishableKey: "sb_publishable_wXoQC8djBvfiLYK1eX-h_Q_uFnGTAPB",
-  reportRpc: "get_business_status_report",
+  reportRpc: "get_business_status_report_v2",
   refreshIntervalMs: 60000
 };
