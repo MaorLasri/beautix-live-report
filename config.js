d18b3a6120f28678ba03f529610696d10c40c819
MaorLasri <39355371+MaorLasri@users.noreply.github.com>
2026-07-30 12:21:34 -0700

    Use latest business status RPC in test dashboard

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.BEAUTIX_CONFIG = {
   supabaseUrl: "https://bkosuztxdgvfhykzuigx.supabase.co",
   supabasePublishableKey: "sb_publishable_wXoQC8djBvfiLYK1eX-h_Q_uFnGTAPB",
-  reportRpc: "get_business_status_report",
+  reportRpc: "get_business_status_report_v3",
   refreshIntervalMs: 60000
 };
 
