960bd187f9f2979033f234a332216ab117b989d9
MaorLasri <39355371+MaorLasri@users.noreply.github.com>
2026-08-02 20:59:35 -0700

    test-v2: add isolated application config

---
+++ test-v2-config.js
@@ -0,0 +1,8 @@
+window.BEAUTIX_V2_CONFIG = Object.freeze({
+  environment: "test-v2",
+  supabaseUrl: "https://bkosuztxdgvfhykzuigx.supabase.co",
+  supabasePublishableKey: "sb_publishable_wXoQC8djBvfiLYK1eX-h_Q_uFnGTAPB",
+  defaultTab: "overview",
+  defaultPeriodMode: "month",
+  timezone: "Asia/Jerusalem"
+});
